7b7fe37361d3e9408835fe353a21d19b848f0fff
John Raymond Collado <jrcollado1987@gmail.com>
2014-06-28 03:48:32 -0500

    Changes made by John Raymond Collado (jrcollado1987@gmail.com).

--- platform-friends-hybrid/scripts/app/app.js
+++ platform-friends-hybrid/scripts/app/app.js
@@ -168,3 +168,39 @@ var app = (function (win) {
     };
 
 }(window));
+
+(function (g) {
+
+	var productId = "e409f06f532d48c495b12cc6d06f27ba"; // App unique product key
+
+	// Make analytics available via the window.analytics variable
+	// Start analytics by calling window.analytics.Start()
+	var analytics = g.analytics = g.analytics || {};
+	analytics.Start = function () {
+		// Handy shortcuts to the analytics api
+		var factory = window.plugins.EqatecAnalytics.Factory;
+		var monitor = window.plugins.EqatecAnalytics.Monitor;
+		// Create the monitor instance using the unique product key for Analytics
+		var settings = factory.CreateSettings(productId);
+		settings.LoggingInterface = factory.CreateTraceLogger();
+		factory.CreateMonitorWithSettings(settings,
+			function () {
+				console.log("Monitor created");
+				// Start the monitor inside the success-callback
+				monitor.Start(function () {
+					console.log("Monitor started");
+				});
+			},
+			function (msg) {
+				console.log("Error creating monitor: " + msg);
+			}
+		);
+	};
+	analytics.Stop = function () {
+		var monitor = window.plugins.EqatecAnalytics.Monitor;
+		monitor.Stop();
+	};
+	analytics.Monitor = function () {
+		return window.plugins.EqatecAnalytics.Monitor;
+	};
+})(window);
